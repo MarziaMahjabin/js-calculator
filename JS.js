703b6de1662b355e67427ee85009c647c354f70e
Mahjabin Tonni <72104560+MarziaMahjabin@users.noreply.github.com>
2022-07-04 18:26:32 +0600

    Add files via upload

---
+++ JS.js
@@ -0,0 +1,12 @@
+
+
+
+
+
+
+
+document.querySelector("#number:nth-child(1) ,#number:nth-child(2) ,#number:nth-child(3) ,#number:nth-child(4) ").addEventListener( "click", function () {
+
+  alert("sdfsd");
+
+});
